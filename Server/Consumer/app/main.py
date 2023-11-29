c9e03db1a931723b95694c091d4567ab5e9b5939
NathanLenas <nathanlenas@protonmail.com>
2023-11-29 16:26:57 +0100

    SQL Connection on the consumer script, creation of the table at startup

--- Server/Consumer/app/main.py
+++ Server/Consumer/app/main.py
@@ -1,7 +1,8 @@
 from kafka import KafkaConsumer
+import psycopg2
 import time
 
-def connect_with_retry():
+def connect_to_kafka_with_retry():
     while True:
         try:
             consumer = KafkaConsumer('coordinates', bootstrap_servers=['kafka:9092'])
@@ -11,15 +12,46 @@ def connect_with_retry():
             print("Retrying in 5 seconds...")
             time.sleep(5)
 
-consumer = connect_with_retry()
+def connect_to_postgresql_db():
+    while True:
+        try:
+            db = psycopg2.connect("dbname=gpsdb user=user password=password host=postgres")
+            return db
+        except Exception as e:
+            print(f"Connection failed: {e}")
+            print("Retrying in 5 seconds...")
+            time.sleep(5)
+    
 
+consumer = connect_to_kafka_with_retry()
 
-while True:
-    print("Waiting for messages...")
-    for message in consumer:
-        print(message.value)
-        time.sleep(1)
-    time.sleep(1)
+db = connect_to_postgresql_db()
+
+print("----------------------------------------")
+print("Connected to Kafka and PostgreSQL")
+commands = [
+    "CREATE TABLE gps_coordinates (IP VARCHAR(20),LAT FLOAT,LONG FLOAT,timestamp TIMESTAMP,PRIMARY KEY (IP, timestamp));"
+]
+
+try:
+    cur = db.cursor()
+    for command in commands:
+        print("Executing command: " + command)
+        cur.execute(command)
+    cur.close()
+    db.commit()
+except Exception as e:
+    print(f"Error: {e}")
+    db.rollback()
+finally:
+    # while True:
+    #     print("Waiting for messages...")
+    #     for message in consumer:
+    #         print(message.value)
+    #         time.sleep(1)
+    #     time.sleep(1)
+    db.close()
+
     
         
     
