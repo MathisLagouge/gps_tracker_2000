c440fbc53b8d88c9980bb723c0979a4535c6818e
MathisLagouge <lagougemat@cy-tech.fr>
2023-11-29 15:07:36 +0100

    debogage tuple type

--- Client/coordinates.py
+++ Client/coordinates.py
@@ -3,19 +3,19 @@ import random
 
 #init coordinates (lat,long)
 #return random coordinates
-def init_coord() -> tuple(float,float):
+def init_coord() -> tuple((float,float)):
 
     return (random.uniform(-90,90), random.uniform(-180,180))
 
 #init speed
 #return speed vector, [-1 degres,1 degres]
-def init_speed() -> tuple(float,float):
+def init_speed() -> tuple((float,float)):
     
     return (random.uniform(-1,1), random.uniform(-1,1))
 
 # move
 # return new position
-def move(coordinates: tuple(float,float), speed: tuple(float,float)) -> tuple(float,float):
+def move(coordinates: tuple((float,float)), speed: tuple((float,float))) -> tuple((float,float)):
 
     latitude : float = coordinates[0] + speed[0]
     longitude : float = coordinates[1] + speed[1]
@@ -33,4 +33,3 @@ def move(coordinates: tuple(float,float), speed: tuple(float,float)) -> tuple(fl
         latitude += 360
 
     return((latitude, longitude))
-
